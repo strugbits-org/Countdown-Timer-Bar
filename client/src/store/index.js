b9159cbf2ceb8dd2d4e979ce519a34e5e2fd9484
Monis Hasan Khan <59013221+monis8khan@users.noreply.github.com>
2021-06-12 14:27:51 +0500

    roll back to start

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,10 @@
-let apiURL = "http://localhost:3001/admin/gettimer" // http://localhost:3001
+let apiURL = "https://localhost:3001/admin/gettimer" // http://localhost:3001
 
 // Get Tiemr List
 export const getTimerList = async () => {
    const apiData = await fetch(apiURL)
    console.log('Data - - - - - - - >',apiData)
-   debugger
    const data = await apiData.json()
-   console.log('Data - - - - - - - >',data)
  
    return data
 } 
