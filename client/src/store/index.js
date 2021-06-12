35e46ae9798eaa0ebb3ac5d71df8e2a6d6af8ecf
Monis Hasan Khan <59013221+monis8khan@users.noreply.github.com>
2021-06-12 12:15:21 +0500

    http is removed and changed to countdown-timer-app-backend for request

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,4 @@
-let apiURL = "http://countdown-timer-bar-backend/admin/gettimer" // http://localhost:3001
+let apiURL = "countdown-timer-bar-backend/admin/gettimer" // http://localhost:3001
 
 // Get Tiemr List
 export const getTimerList = async () => {
