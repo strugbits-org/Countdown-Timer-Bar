530372089ad8cc4157c8e04082aa739eced840cd
Monis Hasan Khan <59013221+monis8khan@users.noreply.github.com>
2021-06-16 18:07:59 +0500

    fetch from backend

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,4 +1,5 @@
 let apiURL = "http://countdown-timer-bar-backend:8080/admin/gettimer" // http://localhost:3001
+//-- updating this line
 
 // Get Tiemr List
 export const getTimerList = async () => {
